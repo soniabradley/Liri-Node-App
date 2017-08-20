908c4856eee6236cfcd0e304654cbe9ebaf08adc
soniab <sonia.bradley524@gmail.com>
2017-08-20 00:21:58 -0500

    tweeter is tweeting

---
+++ liri.js
@@ -0,0 +1,15 @@
+
+var keys = require('./keys.js')
+
+var Twitter = require("twitter");
+var client = new Twitter(keys.twitterKeys);
+
+var params = {screen_name: 'mona03576'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+console.log('Hello');
+  if (!error) {
+    console.log(tweets[0].text);
+  }else if(error) {
+    console.log(error);
+  }
+});
