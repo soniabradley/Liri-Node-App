af8d09e151b4ded6ae0f1f348f87f922a184c90b
soniab <sonia.bradley524@gmail.com>
2018-01-26 13:21:59 -0600

    key.js added

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# liri-node-app
+# LIRI-Bot
+LIRI is a command line, Node.js app that takes in parameters and gives back data for movies, music, and your personal Twitter account
 
+* .gitignore, important to create this file before "git push" to avoid a any history.
 * npm init, when starting a project, creates package.json.
 * --save, to install packages in local folder and save to package.json, populates node_modules.
 * npm install, installs the dependencies in the local node_modules folder.
-* .gitignore, important to create this file before "git push" to avoid a history.
+
--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded'); 
 
 exports.twitterKeys = {
-  consumer_key: 'fyJMpTDthRKBEBfmN8zB3NhNP',
-  consumer_secret: '7Uqopi3CluIrw3tjNi21FOg7UI2kRQhX9YHdjqnJWYnpI0Ga5m',
-  access_token_key: '898630283452272641PksIeKz5eJXQiYdLIeo7wXKSCFgJEy5',
-  access_token_secret: 'J1oQ2qs4udMIhDRg9GjA5hnVOLuj3myoelQJ9l0O7DCal',
+  consumer_key: 'Xhc7idspamf7HJM6lcUsz61yN',
+  consumer_secret: 'kAY5h3EbrNApHCkT1s2daiOaJ5wQekrt4EJecmIJucnRoiGA6s',
+  access_token_key: '898630283452272641-83y3c3Fi1aAFcaOi0F2KnbMZ3OE4QQZ',
+  access_token_secret: 'vXxROfew7dREngTSoX4e4g88a8gX30uFea3q2pAuab7IV'
 }
