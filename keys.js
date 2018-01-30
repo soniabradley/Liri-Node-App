1dc583fc58273f4f2941280b0748a09713cad07b
soniab <sonia.bradley524@gmail.com>
2018-01-29 22:06:15 -0600

    spotify and request

--- keys.js
+++ keys.js
@@ -1,8 +1,10 @@
-console.log('this is loaded'); 
+console.log('keys loaded'); 
 
 exports.twitterKeys = {
-  consumer_key: 'Xhc7idspamf7HJM6lcUsz61yN',
-  consumer_secret: 'kAY5h3EbrNApHCkT1s2daiOaJ5wQekrt4EJecmIJucnRoiGA6s',
-  access_token_key: '898630283452272641-83y3c3Fi1aAFcaOi0F2KnbMZ3OE4QQZ',
-  access_token_secret: 'vXxROfew7dREngTSoX4e4g88a8gX30uFea3q2pAuab7IV'
+  consumer_key: 'gP3ZHcbjKuI39vL3WmwIDskIZ',
+  consumer_secret: 'ZczvGSLt3QlDLgJW6AfgSnZ06WMbqYU3jzXraBEzgNGaw6od06',
+  access_token_key: '898630283452272641-PksIeKz5eJXQiYdLIeo7wXKSCFgJEy5',
+  access_token_secret: 'J1oQ2qs4udMIhDRg9GjA5hnVOLuj3myoelQJ9l0O7DCal'
 }
+
+// http://www.omdbapi.com/?i=tt3896198&apikey=8055b4
--- liri.js
+++ liri.js
@@ -1,89 +1,99 @@
+// ./ tells javascript to look in the current (same) folder for keys.js
+var keys = require('./keys.js')
 
-var keys = require('./keys1.js')
- 
 var Twitter = require('twitter');
 
 var spotify = require('spotify');
 
 var request = require('request');
 
-// get my-tweets inside a function
-var getMyTweets = function() {
-
+// https://apps.twitter.com/
+var getMyTweets = function () {
 	var client = new Twitter(keys.twitterKeys);
 
-	var params = {screen_name: 'mona03576'};
-	client.get('statuses/user_timeline', params, 
-		function(error, tweets, response) {
-	// console.log('Hello');
-		 if (!error) {
-		  for(var i=0; i<tweets.length; i++) {
-		  	console.log(tweets[i].created_at);
-		  	console.log(' ');
-		    console.log(tweets[i].text);
-  	}
-  }
-});
+	var params = {
+		screen_name: 'mona03576'
+	};
+	client.get('statuses/user_timeline', params,
+		function (error, tweets, response) {
+			if (!error) {
+				//  console.log(tweets);
+				for (var i = 0; i < tweets.length; i++) {
+					console.log(tweets[i].created_at);
+					console.log(' ');
+					console.log(tweets[i].text);
+				}
+			}
+		});
 }
-
-var getArtistName = function(artist) {
+// Twitter End
+// Spotify Start
+var getArtistNames = function (artist) {
 	return artist.name;
 }
+var getMeSpotify = function (songName) {
 
-var getMeSpotify = function(songName){
-//start of spotify copy and paste the search from npm package
-	spotify.search({ type: 'track', query: songName }, function(err, data) {
-	    if ( err ) {
-	        console.log('Error occurred: ' + err);
-	        return;
-	    }
-	    var songs = data.tracks.items;
-	    for(var i=0; i<songs.length; i++) {
-	    	console.log(i);
-	    	console.log('artist(s): '+ songs[i].artist.map(
-	    		getArtistNames));
-	    	console.log('song name: ' + songs[i].name);
-	    	console.log('preview song: ' + songs[i].preview_url);
-	    	console.log('album: ' + songs[i].album.name);
-	    	console.log('-----------------');
-	    }
+	spotify.search({
+		type: 'track',
+		query: songName
+	}, function (err, data) {
+		if (err) {
+			console.log('Error occurred: ' + err);
+			return;
+		}
+
+		var songs = data.tracks.items;
+		for (var i = 0; i < songs.length; i++) {
+			// console.log(data.tracks.items[0]);
+			console.log(i);
+			console.log('artist(s): ' + songs[i].artist.map(
+				getArtistNames));
+			console.log('song name: ' + songs[i].name);
+			console.log('preview song: ' + songs[i].preview_url);
+			console.log('album: ' + songs[i].album.name);
+			console.log('-----------------');
+		}
 	});
 }
-// end of spotify
-var getMeMovie = function(movieName) {
+// Spotify End
+// Request Start
+var getMeMovie = function (movieName) {
 
 	request('http://www.http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json', function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	   
-	   	var jsonData = JSON.parse(body);
+		if (!error && response.statusCode == 200) {
+			// console.log(body)
+			var jsonData = JSON.parse(body);
 
-	   	console.log('Title:' + jsonData.Title);
-	   	console.log('Year: ' + jsonData.Year);
-	   	console.log('Rated: ' + jsonData.Rated);
-	   	console.log('Actors: ' + jsonData.Actors);
-	   	console.log('Rotten tomatoes rating:' + jsonData.tomatoRaing);
-	   
-	}
-});
+			console.log('Title: ' + jsonData.Title);
+			console.log('Year: ' + jsonData.Year);
+			console.log('Rated: ' + jsonData.Rated);
+			console.log('Actors: ' + jsonData.Actors);
+			console.log('Rotten tomatoes rating:' + jsonData.tomatoRating);
+
+		}
+	})
 }
+// Request End
+// fs start
 var doWhatItSays = function () {
-	fs.readFile('random.txt', 'utf8', function (err, data)  {
-	  if (err) throw err;
- 	
-	  	var dataArr = data.split(',');
+	fs.readFile('random.txt', 'utf8', function (err, data) {
+		if (err) throw err;
 
-	  	if (dataArr.length == 2) {
-	  		pick(dataArr[0], dataArr[1]);
+		var dataArr = data.split(',');
+
+		if (dataArr.length == 2) {
+			pick(dataArr[0], dataArr[1]);
 		} else if (dataArr.length == 1) {
 			pick(dataArr[0]);
 		}
 
 	});
 }
-// switch statement hold different arguments for user
-var pick = function(caseData, functionData) {
-	switch(caseData) {
-		case 'my-tweets' :
+// fs end
+// Switch Statements hold different arguments for user
+var pick = function (caseData, functionData) {
+	switch (caseData) {
+		case 'my-tweets':
 			getMyTweets();
 			break;
 		case 'spotify-this-song':
@@ -91,19 +101,19 @@ var pick = function(caseData, functionData) {
 			break;
 		case 'movie-this':
 			getMeMovie(functionData);
-		case 'do-what-it-say':
+		case 'do-what-it-says':
 			doWhatItSays();
+			break;
 		default:
-		// if command is entered that liri does not recognize
-		console.log('Liri does not know');
+
+			// if command is entered that liri does not recognize
+			console.log('LIRI does not know');
 	}
 }
-// function to pass arguments into when I run pick function
-var runThis = function(argOne, argTwo) {
+// function to pass arguments when I run pick function
+var runThis = function (argOne, argTwo) {
 	pick(argOne, argTwo);
 };
-// process argv live inside 0f the array, first item in array is ndc0=node, the second item is the ndc1=file name.
+// first item in array is ndc0=node, the second item is the ndc1=file name.
 // argv2=my-tweets or spotify-this-song, argv3 is the name of song or tweet.
 runThis(process.argv[2], process.argv[3]);
-
-//now enter spotify package on line 23
